d050fca1a1217133163d5dab8d0c3c120604bf35
Harsh Mishra <143594551+HarshMishra-Git@users.noreply.github.com>
2025-02-01 19:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def login():
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=port)
+# app.run(debug=True)
